11671280d07a3bc38e3f1ce36a4992709bfabeaa
RyanLil-Xvx <91490019+RyanLil-Xvx@users.noreply.github.com>
2024-02-07 23:36:06 -0500

    Update transactions_rds_processing.py and ipynb version
    Update transactions_rds_processing.py and also the graph generate by the file. All information include in a ipynb file.

--- code/transactions_rds_processing.py
+++ code/transactions_rds_processing.py
@@ -5,21 +5,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 '''
-[1] Histograms of Feature Distributions:
-
-Helping visualize the distribution of individual features. Understanding the distribution is essential for identifying potential outliers, detecting skewness, and gaining insights into the overall data structure.
-
-[2] Boxplot of Feature Distributions:
-
-Boxplots provide a concise summary of the distribution, helping identify potential outliers and understand the spread of values for each feature.
-
-[3] Checking for Missing Values and get the approximately information of data:
+[1] Checking for Missing Values and get the approximately information of data:
 
 Detecting missing values is crucial for understanding data quality. If there were missing values, specific techniques (like imputation or removal) might be necessary to handle them.
 
-[4] Correlation Heatmap:
+[2] Correlation Heatmap:
 
 Understanding feature correlations is important for feature selection and multicollinearity analysis. A heatmap provides an intuitive representation of the relationships between different features.
+
+[3] Boxplot of Feature Distributions:
+
+Boxplots provide a concise summary of the distribution, helping identify potential outliers and understand the spread of values for each feature.
+
+[4] Histograms of Feature Distributions:
+
+Helping visualize the distribution of individual features. Understanding the distribution is essential for identifying potential outliers, detecting skewness, and gaining insights into the overall data structure.
 '''
 transaction = pyreadr.read_r('/data/IDEA_DeFi_Research/Data/Lending_Protocols/Aave/V2/Mainnet/transactions.rds')
 df = transaction[None]
