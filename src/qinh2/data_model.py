319ae643ae04dc9eb83d1f4f52cf0b4f9371e34a
RyanLil-Xvx <91490019+RyanLil-Xvx@users.noreply.github.com>
2024-03-15 22:11:12 -0400

    Update data_model.py

--- src/qinh2/data_model.py
+++ src/qinh2/data_model.py
@@ -3,7 +3,7 @@ import pyreadr
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
-transaction = pyreadr.read_r("/data/IDEA_DeFi_Research/Data/Lending_Protocols/Aave/V2/Mainnet/transactions.rds")
+transaction = pyreadr.read_r("/Users/ryanlil86/Desktop/RPI/junior2/RCOS/Defi-Analysis/qhz_code_qinh2/local/transactions.rds")
 df = transaction[None]
 df['DateTime'] = df['timestamp'].transform(lambda x: datetime.fromtimestamp(x))
 df.head()
@@ -17,7 +17,7 @@ dailyTransactionCount = dailyTransactionCount[['id']]
 dailyTransactionCount.rename(columns={"id": "transactionCount"}, inplace = True)
 print(dailyTransactionCount)
 # We load the minutely Aave price data here:
-aavePrices = pandas.read_csv('/data/IDEA_DeFi_Research/Data/Coin_Prices/Minutely/aavePrices.csv')
+aavePrices = pandas.read_csv('/Users/ryanlil86/Desktop/RPI/junior2/RCOS/Defi-Analysis/qhz_code_qinh2/local/aavePrices2.csv')
 # And here, since we want to predict daily prices, we create a new features which is the mean daily price.
 aavePrices['DateTime'] = aavePrices['timestamp'].transform(lambda x: datetime.fromtimestamp(x))
 dailyMeanPrices = aavePrices.groupby([df['DateTime'].dt.date]).mean()
@@ -25,9 +25,7 @@ dailyMeanPrices = dailyMeanPrices[['priceUSD']]
 print(dailyMeanPrices)
 dailyTransactionCount = dailyTransactionCount.merge(dailyMeanPrices, left_index = True, right_index = True)
 print(dailyTransactionCount)
-
-# data_split takes in a dataset and returns a list of 4 dataframes: X_train, X_test, y_train, y_test #
-def data_split(data_set):
+def data_split1(data_set):
     from sklearn.model_selection import TimeSeriesSplit
     # We want to use the transactionCount to predict the next day's price. To do this, we "lead" the priceUSD
     # column so in a given row, the transaction count is aligned with the next day's price.
@@ -41,75 +39,6 @@ def data_split(data_set):
     X = data_set.drop(labels=['priceUSD_lead_1', 'dailyPercentChange'],axis=1)
     y = data_set['dailyPercentChange']
     for train_index, test_index in tss.split(data_set):
-        X_train, X_test = X.iloc[train_index, :], X.iloc[test_index,:]
-        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-    return [X_train, X_test, y_train, y_test]
-
-# linear_regression_model takes in the 4 dataframes from data_split and returns a list of predictions and the literal y_test values #
-def linear_regression_model(X_train, X_test, y_train, y_test):
-    from sklearn.linear_model import LinearRegression
-    # We fit a linear model with the train data, where X_train is our feature matrix and y_train is our target variable
-    # using LinearRegression to classify the data
-    estimator = LinearRegression()
-    fit = estimator.fit(X_train, y_train)
-    # We compute the predictions for the X_test features:
-    predictions = fit.predict(X_test)
-    # The line below just computes the average accuracy of our predictions:
-    np.linalg.norm(predictions - y_test) / len(y_test)
-    # This for-loop is super ugly and represents my inexperience with Python. All it is intended to do is get
-    # the literal y_test values without the associated datetimes, for plotting purposes.
-    y_test_vals = list()
-    for i in y_test:
-        y_test_vals.append(i)
-    return predictions, y_test_vals
-
-# plot_ground_truth takes in the predictions and the literal y_test values #
-def plot_ground_truth(predictions, y_test_vals):
-    # We plot the ground-truth values in blue and the predicted values in red:
-    plt.plot(y_test_vals, color = "blue")
-    plt.plot(predictions, color = "red")
-    
-# plot_difference takes in the predictions and the literal y_test values #
-def plot_difference(predictions, y_test_vals):
-    # We plot the difference between our model's predictions and the actual values:
-    plt.plot(y_test_vals - predictions)
-
-def k_neighbors_classifier(X_train, X_test, y_train, y_test):
-    from sklearn.linear_model import KNeighborsClassifier
-    # using KNN to classify the data
-    estimator = KNeighborsClassifier(n_neighbors=3)
-    fit = estimator.fit(X_train, y_train)
-    predictions = fit.predict(X_test)
-    np.linalg.norm(predictions - y_test) / len(y_test)
-    y_test_vals = list()
-    for i in y_test:
-        y_test_vals.append(i)
-    # evaluate the KNN model
-    # method1: compare the real result and predict result
-    y_predict = estimator.predict(X_test)
-    print("y_predict:\n", y_predict)
-    print("compare real result and predict result:\n", y_test == y_predict)
-    
-    # method2: calculate the accuracy
-    accuracy = estimator.score(X_test, y_test)
-    print("accuracy: {0:.2f}\n".format(accuracy * 100))
-    return predictions, y_test_vals
-    
-# We call the functions we defined above here:
-'''
-train_set[0] = X_train
-train_set[1] = X_test
-train_set[2] = y_train
-train_set[3] = y_test
-'''
-train_set = list()
-train_set = data_split(dailyTransactionCount) # store all 4 types of data inside
-# using the linear_regression_model to make prediction
-predictions, y_test_vals = linear_regression_model(train_set[0], train_set[1], train_set[2], train_set[3])
-plot_ground_truth(predictions, y_test_vals)
-plot_difference(predictions, y_test_vals)
-
-# using the k_neighbors_classifier to make prediction
-predictions, y_test_vals = k_neighbors_classifier(train_set[0], train_set[1], train_set[2], train_set[3])
-plot_ground_truth(predictions, y_test_vals)
-plot_difference(predictions, y_test_vals)
+        feature_train, feature_test = X.iloc[train_index, :], X.iloc[test_index,:]
+        target_train, target_test = y.iloc[train_index], y.iloc[test_index]
+    return [feature_train, feature_test, target_train, target_test]
