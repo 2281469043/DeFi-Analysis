eebf784b8bd148577cf68beade54a51c5b2c56a0
Kaiyang Chang <changk2@rpi.edu>
2024-03-18 14:49:58 -0400

    data_split
    data_split function

---
+++ src/changk2/data_model.py
@@ -0,0 +1,66 @@
+import pandas
+import pyreadr
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+transaction = pyreadr.read_r("/Users/ryanlil86/Desktop/RPI/junior2/RCOS/Defi-Analysis/qhz_code_qinh2/local/transactions.rds")
+df = transaction[None]
+df['DateTime'] = df['timestamp'].transform(lambda x: datetime.fromtimestamp(x))
+df.head()
+# We are going to make a super basic linear model to try and predict how the AAVE token's price changes each day.
+# This script will engineer one feature to use for this predictive task: dailyTransactionCount.
+
+# To do this, we group the data by the date portion of the DateTime object, 
+# and then simply count how many transactions are in each group
+dailyTransactionCount = df.groupby([df['DateTime'].dt.date]).count()
+dailyTransactionCount = dailyTransactionCount[['id']]
+dailyTransactionCount.rename(columns={"id": "transactionCount"}, inplace = True)
+print(dailyTransactionCount)
+# We load the minutely Aave price data here:
+aavePrices = pandas.read_csv('/Users/ryanlil86/Desktop/RPI/junior2/RCOS/Defi-Analysis/qhz_code_qinh2/local/aavePrices2.csv')
+# And here, since we want to predict daily prices, we create a new features which is the mean daily price.
+aavePrices['DateTime'] = aavePrices['timestamp'].transform(lambda x: datetime.fromtimestamp(x))
+dailyMeanPrices = aavePrices.groupby([df['DateTime'].dt.date]).mean()
+dailyMeanPrices = dailyMeanPrices[['priceUSD']]
+print(dailyMeanPrices)
+dailyTransactionCount = dailyTransactionCount.merge(dailyMeanPrices, left_index = True, right_index = True)
+print(dailyTransactionCount)
+
+def data_split1(data_set):
+    from sklearn.model_selection import TimeSeriesSplit
+    # We want to use the transactionCount to predict the next day's price. To do this, we "lead" the priceUSD
+    # column so in a given row, the transaction count is aligned with the next day's price.
+    data_set['priceUSD_lead_1'] = data_set['priceUSD'].shift(-1)
+    # We need to drop NA values. One NA value is introduced through this "lead" on the last day in the dataset.
+    data_set.dropna(inplace=True)
+    # In practice, it is better to predict daily percent price changes rather than predicting literal prices, so we compute the daily
+    # percent change here by subtraction tomorrow's price from today's and dividing by today's price.
+    data_set['dailyPercentChange'] = (data_set['priceUSD_lead_1'] - data_set['priceUSD']) / data_set['priceUSD']
+    tss = TimeSeriesSplit(n_splits = 3)
+    X = data_set.drop(labels=['priceUSD_lead_1', 'dailyPercentChange'],axis=1)
+    y = data_set['dailyPercentChange']
+    for train_index, test_index in tss.split(data_set):
+        feature_train, feature_test = X.iloc[train_index, :], X.iloc[test_index,:]
+        target_train, target_test = y.iloc[train_index], y.iloc[test_index]
+    return [feature_train, feature_test, target_train, target_test]
+
+def data_split2(data_set):
+    from sklearn.model_selection import TimeSeriesSplit
+    # We want to use the transactionCount to predict the next day's price. To do this, we "lead" the priceUSD
+    # column so in a given row, the transaction count is aligned with the next day's price.
+    dailyTransactionCount['priceUSD_lead_1'] = dailyTransactionCount['priceUSD'].shift(-1)
+    # We need to drop NA values. One NA value is introduced through this "lead" on the last day in the dataset.
+    dailyTransactionCount.dropna(inplace=True)
+    # In practice, it is better to predict daily percent price changes rather than predicting literal prices, so we compute the daily
+    # percent change here by subtraction tomorrow's price from today's and dividing by today's price.
+    dailyTransactionCount['dailyPercentChange'] = (dailyTransactionCount['priceUSD_lead_1'] - dailyTransactionCount['priceUSD']) / dailyTransactionCount['priceUSD']
+    # We want to predict the direction of the daily percent change, so we create a new feature which is the sign of the daily percent change.
+    dailyTransactionCount['directionOfDailyChange'] = np.sign(dailyTransactionCount['dailyPercentChange'])
+    print(dailyTransactionCount)
+    tss = TimeSeriesSplit(n_splits = 3)
+    X = dailyTransactionCount.drop(labels=['priceUSD_lead_1', 'dailyPercentChange', 'directionOfDailyChange'],axis=1)
+    y = dailyTransactionCount['directionOfDailyChange']
+    for train_index, test_index in tss.split(dailyTransactionCount):
+        feature_train, feature_test = X.iloc[train_index, :], X.iloc[test_index,:]
+        target_train, target_test = y.iloc[train_index], y.iloc[test_index]
+    return [feature_train, feature_test, target_train, target_test]
