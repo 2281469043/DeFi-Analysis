06cc1f4cc82ffb316818716c8d9628b5813e8bea
Yixuan Shen <sheny22@rpi.edu>
2024-03-27 13:24:35 -0400

    Add .DS_Store and data/ to .gitignore, and add python file

---
+++ src/sheny22/sheny22_dp1.py
@@ -0,0 +1,8 @@
+import pyreadr
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from datetime import datetime
+
