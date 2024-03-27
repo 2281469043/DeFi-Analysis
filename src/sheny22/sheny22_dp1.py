d3667ecda9f5c658df196bffc61eb5937ce25abe
Yixuan Shen <sheny22@rpi.edu>
2024-03-27 13:27:37 -0400

    Add code to read transaction data from RDS file

--- src/sheny22/sheny22_dp1.py
+++ src/sheny22/sheny22_dp1.py
@@ -6,3 +6,12 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from datetime import datetime
 
+result = pyreadr.read_r('../../data/transactions.rds')
+
+df = result[None]
+transaction_label = df.columns.tolist()
+
+data_type = df["type"]
+borrow_type = df[df["type"] == "borrow"][["id", "timestamp"]]
+borrow_type.info()
+
