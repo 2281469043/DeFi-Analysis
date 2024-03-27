79b471a9d385a3323a950d76ec33c8de1305f616
Yixuan Shen <sheny22@rpi.edu>
2024-03-27 13:36:33 -0400

    Add date conversion and plot visualization

--- src/sheny22/sheny22_dp1.py
+++ src/sheny22/sheny22_dp1.py
@@ -15,3 +15,14 @@ data_type = df["type"]
 borrow_type = df[df["type"] == "borrow"][["id", "timestamp"]]
 borrow_type.info()
 
+# Convert timestamp to date
+borrow_type["timestamp"] = pd.to_datetime(borrow_type["timestamp"], unit='s')
+borrow_type["date"] = borrow_type["timestamp"].dt.date
+borrow_type = borrow_type.drop(columns=["timestamp"])
+borrow_type.head()
+
+plt.plot(borrow_type["date"].value_counts().sort_index().rolling(window=7).mean())
+# Make the X axis wider
+plt.xticks(rotation=15)
+plt.show()
+
