be299269d2a56b3b27f990f710be5c68e233cab8
RyanLil-Xvx <91490019+RyanLil-Xvx@users.noreply.github.com>
2024-03-28 15:17:14 -0400

    Update data_model2.py

--- src/qinh2/data_model2.py
+++ src/qinh2/data_model2.py
@@ -23,3 +23,25 @@ print(dailyBorrowedAmountsUSD)
 dailyTransactionCount = dailyTransactionCount[['id']]
 dailyTransactionCount.rename(columns={"id": "transactionCount"}, inplace = True)
 print(dailyTransactionCount)
+
+# We load the minutely Aave price data here:
+aavePrices = pandas.read_csv('/data/IDEA_DeFi_Research/Data/Coin_Prices/Minutely/aavePrices.csv')
+# And here, since we want to predict daily prices, we create a new features which is the mean daily price.
+aavePrices['DateTime'] = aavePrices['timestamp'].transform(lambda x: datetime.fromtimestamp(x))
+dailyMeanPrices = aavePrices.groupby([df['DateTime'].dt.date]).mean()
+dailyMeanPrices = dailyMeanPrices[['priceUSD']]
+print(dailyMeanPrices)
+
+'''
+# Stage 2
+In stage 2, our focus shifts towards feature engineering. The **dailyTransactionCount** now
+integrates four distinct types of data: deposit, withdraw, borrow, and repay. Meanwhile, 
+we are currently deliberating on the best approach to incorporate the "liquidation" type 
+data into the **dailyTransactionCount**.
+'''
+# feature engineering, merge dailyTransactionCount, dailyMeanPrices, dailyBorrowedAmountsUSD
+dailyTransactionCount = dailyTransactionCount.merge(dailyMeanPrices, how = "left", on = "DateTime")
+dailyTransactionCount = dailyTransactionCount.merge(dailyBorrowedAmountsUSD, how = "left", on = "DateTime")
+print(dailyTransactionCount)
+
+machine_learning_model_record = dict()
