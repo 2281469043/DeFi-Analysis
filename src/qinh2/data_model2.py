15d86520fefd0b4bddf9a662eee87e88380b678f
RyanLil-Xvx <91490019+RyanLil-Xvx@users.noreply.github.com>
2024-03-28 15:18:16 -0400

    Update data_model2.py

--- src/qinh2/data_model2.py
+++ src/qinh2/data_model2.py
@@ -45,3 +45,34 @@ dailyTransactionCount = dailyTransactionCount.merge(dailyBorrowedAmountsUSD, how
 print(dailyTransactionCount)
 
 machine_learning_model_record = dict()
+
+def data_split2(data_set):
+    from sklearn.model_selection import TimeSeriesSplit
+    # We want to use the transactionCount to predict the next day's price. To do this, we "lead" the priceUSD
+    # column so in a given row, the transaction count is aligned with the next day's price.
+    data_set['priceUSD_lead_1'] = data_set['priceUSD'].shift(-1)
+    # We need to drop NA values. One NA value is introduced through this "lead" on the last day in the dataset.
+    data_set.dropna(inplace=True)
+    # In practice, it is better to predict daily percent price changes rather than predicting literal prices, so we compute the daily
+    # percent change here by subtraction tomorrow's price from today's and dividing by today's price.
+    data_set['dailyPercentChange'] = (data_set['priceUSD_lead_1'] - data_set['priceUSD']) / data_set['priceUSD']
+    # We want to predict the direction of the daily percent change, so we create a new feature which is the sign of the daily percent change.
+    data_set['directionOfDailyChange'] = np.sign(data_set['dailyPercentChange'])
+    print(data_set)
+    tss = TimeSeriesSplit(n_splits = 3)
+    X = data_set.drop(labels=['priceUSD_lead_1', 'dailyPercentChange', 'directionOfDailyChange'],axis=1)
+    y = data_set['directionOfDailyChange']
+    for train_index, test_index in tss.split(data_set):
+        feature_train, feature_test = X.iloc[train_index, :], X.iloc[test_index,:]
+        target_train, target_test = y.iloc[train_index], y.iloc[test_index]
+    return [feature_train, feature_test, target_train, target_test]
+
+def plot_ground_truth(predictions, target_test_vals):
+    # We plot the ground-truth values in blue and the predicted values in red:
+    plt.plot(target_test_vals, color = "blue")
+    plt.plot(predictions, color = "red")
+
+def plot_difference(predictions, y_test_vals):
+    # We plot the difference between our model's predictions and the actual values:
+    plt.plot(y_test_vals - predictions)
+    
