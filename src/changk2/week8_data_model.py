1450e5bc1fb6aa9117ba7f35b4b11d48051b0398
Kaiyang Chang <changk2@rpi.edu>
2024-03-29 22:50:16 -0400

    Update week8_data_model.py

--- src/changk2/week8_data_model.py
+++ src/changk2/week8_data_model.py
@@ -46,3 +46,41 @@ aavePrices['DateTime'] = aavePrices['timestamp'].transform(lambda x: datetime.fr
 dailyMeanPrices = aavePrices.groupby([df['DateTime'].dt.date]).mean()
 dailyMeanPrices = dailyMeanPrices[['priceUSD']]
 print(dailyMeanPrices)
+
+'''
+# Stage 2
+In stage 2, our focus shifts towards feature engineering. The **dailyTransactionCount** now
+integrates four distinct types of data: deposit, withdraw, borrow, and repay. Meanwhile, 
+we are currently deliberating on the best approach to incorporate the "liquidation" type 
+data into the **dailyTransactionCount**.
+'''
+# feature engineering, merge dailyTransactionCount, dailyMeanPrices, dailyBorrowedAmountsUSD
+dailyTransactionCount = dailyTransactionCount.merge(dailyMeanPrices, how = "left", on = "DateTime")
+dailyTransactionCount = dailyTransactionCount.merge(dailyBorrowsAmountsUSD, how = "left", on = "DateTime")
+dailyTransactionCount = dailyTransactionCount.merge(dailyDepositsAmountsUSD, how = "left", on = "DateTime")
+dailyTransactionCount = dailyTransactionCount.merge(dailyRepaysAmountsUSD, how = "left", on = "DateTime")
+dailyTransactionCount = dailyTransactionCount.merge(dailyWithdrawsAmountsUSD, how = "left", on = "DateTime")
+print(dailyTransactionCount)
+
+machine_learning_model_record = dict()
+
+def data_split2(data_set):
+    from sklearn.model_selection import TimeSeriesSplit
+    # We want to use the transactionCount to predict the next day's price. To do this, we "lead" the priceUSD
+    # column so in a given row, the transaction count is aligned with the next day's price.
+    data_set['priceUSD_lead_1'] = data_set['priceUSD'].shift(-1)
+    # We need to drop NA values. One NA value is introduced through this "lead" on the last day in the dataset.
+    data_set.dropna(inplace=True)
+    # In practice, it is better to predict daily percent price changes rather than predicting literal prices, so we compute the daily
+    # percent change here by subtraction tomorrow's price from today's and dividing by today's price.
+    data_set['dailyPercentChange'] = (data_set['priceUSD_lead_1'] - data_set['priceUSD']) / data_set['priceUSD']
+    # We want to predict the direction of the daily percent change, so we create a new feature which is the sign of the daily percent change.
+    data_set['directionOfDailyChange'] = np.sign(data_set['dailyPercentChange'])
+    print(data_set)
+    tss = TimeSeriesSplit(n_splits = 3)
+    X = data_set.drop(labels=['priceUSD_lead_1', 'dailyPercentChange', 'directionOfDailyChange'],axis=1)
+    y = data_set['directionOfDailyChange']
+    for train_index, test_index in tss.split(data_set):
+        feature_train, feature_test = X.iloc[train_index, :], X.iloc[test_index,:]
+        target_train, target_test = y.iloc[train_index], y.iloc[test_index]
+    return [feature_train, feature_test, target_train, target_test]
