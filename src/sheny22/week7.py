eb3c1139aafcbc18f4bf0f8882eddce3a5ee3cf4
Yixuan Shen <sheny22@rpi.edu>
2024-04-02 16:43:10 -0400

    Add data manipulation for borrow transactions

--- src/sheny22/week7.py
+++ src/sheny22/week7.py
@@ -8,7 +8,20 @@ df = transaction_rds[None]
 df.plot() # plot the data
 df_pandas = pd.DataFrame(df) # set to pandas data frame
 transaction_rds_data_label = df_pandas.columns.tolist()
-# print(transaction_rds_data_label) # use to check the data label
 df_pandas.head() # check the basic data, know the sample of data
 
+data_type = df_pandas["type"]
+data_type.value_counts().plot(kind='bar')
 
+borrow_type = df_pandas[df_pandas["type"] == "borrow"]
+borrow_type = borrow_type[["id", "timestamp"]]
+borrow_type.info()
+
+borrow_type["timestamp"] = pd.to_datetime(borrow_type["timestamp"], unit='s')
+borrow_type.head()
+
+borrow_type["date"] = borrow_type["timestamp"].dt.date
+borrow_type.head()
+
+borrow_type = borrow_type.drop(columns=["timestamp"])
+borrow_type.head()
