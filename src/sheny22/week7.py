46ddbe96922fb62bbf3de61bbf52ec083723a563
Yixuan Shen <sheny22@rpi.edu>
2024-04-02 16:43:45 -0400

    Add analysis graph to visualize borrow type data

--- src/sheny22/week7.py
+++ src/sheny22/week7.py
@@ -25,3 +25,28 @@ borrow_type.head()
 
 borrow_type = borrow_type.drop(columns=["timestamp"])
 borrow_type.head()
+
+plt.plot(borrow_type["date"].value_counts().sort_index())
+# Format x-axis to display dates in "YYYY-MM-DD" format
+plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%Y-%m-%d'))
+plt.xticks(rotation=45)  # rotate x-axis labels for better readability
+plt.grid(True)
+plt.show()
+
+# create a 'DateTime' column from the 'timestamp' column 
+df_pandas['DateTime'] = df_pandas['timestamp'].transform(lambda x: datetime.datetime.fromtimestamp(x))
+df_pandas.head() # check the updated data
+
+# start to draw the graph set x_axis is DateTime and y_axis is amount
+df_pandas['DateTime'] = pd.to_datetime(df_pandas['DateTime'])
+
+# convert DataFrame columns to numpy arrays
+x = df_pandas["DateTime"].values
+y = df_pandas["amountUSD"].values
+
+# plot the graph
+plt.plot(x, y)
+plt.title("Analysis Graph")
+plt.xlabel("DateTime")
+plt.ylabel("Amount")
+plt.show()
