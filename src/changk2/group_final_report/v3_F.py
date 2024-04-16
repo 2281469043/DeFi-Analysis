54081886304b71741bd017bcb5f9958b0e02b2eb
Kaiyang Chang <changk2@rpi.edu>
2024-04-16 02:33:09 -0400

    Update v3_F.py

--- src/changk2/group_final_report/v3_F.py
+++ src/changk2/group_final_report/v3_F.py
@@ -76,3 +76,131 @@ def transaction_v3_Fantom():
     
     return dailyTransactionCount
 
+def knn_model_gridSearchCV(feature_train, feature_test, target_train, target_test):
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.preprocessing import StandardScaler
+    transfer = StandardScaler()
+    # train data standardization
+    feature_train = transfer.fit_transform(feature_train)
+    feature_test = transfer.transform(feature_test)
+    estimator = KNeighborsClassifier(n_neighbors=3, weights="uniform", algorithm="auto", leaf_size=30, p=2, metric="minkowski", metric_params=None, n_jobs=None)
+    # KNN model optimization
+    estimator = KNeighborsClassifier()
+    parameters_testcase = {"n_neighbors": [3, 5, 7, 9, 11, 13]}
+    estimator = GridSearchCV(estimator, parameters_testcase, cv=3)
+    estimator.fit(feature_train, target_train)
+    # We compute the predictions for the feature_test features:
+    predictions = estimator.predict(feature_test)
+    # The line below just computes the average accuracy of our predictions:
+    np.linalg.norm(predictions - target_test) / len(target_test)
+    target_test_vals = list()
+    for data in target_test:
+        target_test_vals.append(data)
+    # model evaluation
+    target_predict = estimator.predict(feature_test)
+    print("-------------------- knn with gridSearchCV --------------------\n")
+    print("The target_predict is:\n", target_predict)
+    print("Compare predicted results with actual values:\n", target_predict == target_test)
+    accuracy = estimator.score(feature_test, target_test) * 100
+    print("Accuracy:\n{0:.2f}%".format(accuracy))
+    return predictions, target_test_vals, accuracy
+
+dailyTransactionCount_v3_Fantom = transaction_v3_Fantom()
+feature_train, feature_test, target_train, target_test = data_split(dailyTransactionCount_v3_Fantom)
+predictions, target_test_vals, accuracy = knn_model_gridSearchCV(feature_train, feature_test, target_train, target_test)
+# make record for the accuracy
+machine_learning_model_record["knn_gridSearch"] = accuracy
+plot_ground_truth(predictions, target_test_vals)
+plot_difference(predictions, target_test_vals)
+
+def multinomialNB_model(feature_train, feature_test, target_train, target_test):
+    from sklearn.naive_bayes import MultinomialNB
+    estimator = MultinomialNB(alpha=1.0, fit_prior=True, class_prior=None)
+    fit = estimator.fit(feature_train, target_train)
+    # We compute the predictions for the feature_test features:
+    predictions = fit.predict(feature_test)
+    # The line below just computes the average accuracy of our predictions:
+    np.linalg.norm(predictions - target_test) / len(target_test)
+    target_test_vals = list()
+    for data in target_test:
+        target_test_vals.append(data)
+    # model evaluation
+    target_predict = estimator.predict(feature_test)
+    print("-------------------- naive bayes --------------------\n")
+    print("The target_predict is:\n", target_predict)
+    print("Compare predicted results with actual values:\n", target_predict == target_test)
+    accuracy = estimator.score(feature_test, target_test) * 100
+    print("Accuracy:\n{0:.2f}%".format(accuracy))
+    return predictions, target_test_vals, accuracy
+
+dailyTransactionCount_v3_Fantom = transaction_v3_Fantom()
+feature_train, feature_test, target_train, target_test = data_split(dailyTransactionCount_v3_Fantom)
+predictions, target_test_vals, accuracy = multinomialNB_model(feature_train, feature_test, target_train, target_test)
+# make record for the accuracy
+machine_learning_model_record["naive_bayes"] = accuracy
+plot_ground_truth(predictions, target_test_vals)
+plot_difference(predictions, target_test_vals)
+
+def decision_tree_model(feature_train, feature_test, target_train, target_test):
+    from sklearn.tree import DecisionTreeClassifier
+    estimator = DecisionTreeClassifier(criterion="gini", splitter="best", max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, min_impurity_decrease=0.0)
+    fit = estimator.fit(feature_train, target_train)
+    # We compute the predictions for the feature_test features:
+    predictions = fit.predict(feature_test)
+    # The line below just computes the average accuracy of our predictions:
+    np.linalg.norm(predictions - target_test) / len(target_test)
+    target_test_vals = list()
+    for data in target_test:
+        target_test_vals.append(data)
+    # model evaluation
+    target_predict = estimator.predict(feature_test)
+    print("-------------------- decision tree --------------------\n")
+    print("The target_predict is:\n", target_predict)
+    print("Compare predicted results with actual values:\n", target_predict == target_test)
+    accuracy = estimator.score(feature_test, target_test) * 100
+    print("Accuracy:\n{0:.2f}%".format(accuracy))
+    return predictions, target_test_vals, accuracy
+
+dailyTransactionCount_v3_Fantom = transaction_v3_Fantom()
+feature_train, feature_test, target_train, target_test = data_split(dailyTransactionCount_v3_Fantom)
+predictions, target_test_vals, accuracy = decision_tree_model(feature_train, feature_test, target_train, target_test)
+# make record for the accuracy
+machine_learning_model_record["decision_tree"] = accuracy
+plot_ground_truth(predictions, target_test_vals)
+plot_difference(predictions, target_test_vals)
+
+def random_forest_model_gridSearchCV(feature_train, feature_test, target_train, target_test):
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.model_selection import GridSearchCV
+    estimator = RandomForestClassifier()
+    param_dict = {"n_estimators": [120,200,300,500,800,1200], "max_depth": [5,8,15,25,30]}
+    estimator = GridSearchCV(estimator, param_grid=param_dict, cv=3)
+    estimator.fit(feature_train, target_train)
+    # We compute the predictions for the feature_test features:
+    predictions = estimator.predict(feature_test)
+    # The line below just computes the average accuracy of our predictions:
+    np.linalg.norm(predictions - target_test) / len(target_test)
+    target_test_vals = list()
+    for data in target_test:
+        target_test_vals.append(data)
+    # model evaluation
+    target_predict = estimator.predict(feature_test)
+    print("-------------------- random forest --------------------\n")
+    print("The target_predict is:\n", target_predict)
+    print("Compare predicted results with actual values:\n", target_predict == target_test)
+    accuracy = estimator.score(feature_test, target_test) * 100
+    print("Accuracy:\n{0:.2f}%".format(accuracy))
+    return predictions, target_test_vals, accuracy
+dailyTransactionCount_v3_Fantom = transaction_v3_Fantom()
+feature_train, feature_test, target_train, target_test = data_split(dailyTransactionCount_v3_Fantom)
+predictions, target_test_vals, accuracy = random_forest_model_gridSearchCV(feature_train, feature_test, target_train, target_test)
+# make record for the accuracy
+machine_learning_model_record["random_forest_gridSearchCV"] = accuracy
+plot_ground_truth(predictions, target_test_vals)
+plot_difference(predictions, target_test_vals)
+
+# model accuracy record
+for i in machine_learning_model_record.keys():
+    print("-" * 60)
+    print("The accuracy of model: {} | {:.2f}%\n".format(i, machine_learning_model_record.get(i)))
