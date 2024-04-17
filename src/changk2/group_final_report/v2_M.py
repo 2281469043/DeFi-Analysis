92772e5be85895764182f00d8fd352db0f993443
Kaiyang Chang <changk2@rpi.edu>
2024-04-17 04:26:38 -0400

    Create v2_M.py

---
+++ src/changk2/group_final_report/v2_M.py
@@ -0,0 +1,77 @@
+import pandas
+import pyreadr
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+def transaction_v2_Mainnet():
+    transaction = pyreadr.read_r("/data/IDEA_DeFi_Research/Data/Lending_Protocols/Aave/V2/Mainnet/transactions.rds")
+    df = transaction[None]
+    df['DateTime'] = df['timestamp'].transform(lambda x: datetime.fromtimestamp(x))
+    df.head()
+    # We are going to make a super basic linear model to try and predict how the AAVE token's price changes each day.
+    # This script will engineer one feature to use for this predictive task: dailyTransactionCount.
+
+    # To do this, we group the data by the date portion of the DateTime object, 
+    # and then simply count how many transactions are in each group
+    dailyTransactionCount = df.groupby([df['DateTime'].dt.date]).count()
+    
+    # Add the liquidations type to the dataframe
+    liquidations = df[df['type'] == "liquidation"]
+    liquidations = liquidations.filter(items = ['DateTime', 'type', 'user', 'principalAmountUSD', 'collateralAmountUSD'], axis = 'columns')
+    liquidations = liquidations.groupby([liquidations['DateTime'].dt.date]).sum()
+    # print(liquidations)
+    
+    # add borrow type to the dataframe
+    borrows = df[df['type'] == "borrow"]
+    dailyBorrowsAmountsUSD = borrows.groupby([borrows['DateTime'].dt.date]).sum()
+    dailyBorrowsAmountsUSD['amountBorrowsUSD'] = dailyBorrowsAmountsUSD['amountUSD']
+    dailyBorrowsAmountsUSD = dailyBorrowsAmountsUSD.filter(items = ['DateTime', 'amountBorrowsUSD'], axis = 'columns')
+    # print(dailyBorrowsAmountsUSD)
+    
+    # add deposit type to the dataframe
+    deposits = df[df['type'] == "deposit"]
+    dailyDepositsAmountsUSD = deposits.groupby([deposits['DateTime'].dt.date]).sum()
+    dailyDepositsAmountsUSD['amountDepositsUSD'] = dailyDepositsAmountsUSD['amountUSD']
+    dailyDepositsAmountsUSD = dailyDepositsAmountsUSD.filter(items = ['DateTime', 'amountDepositsUSD'], axis = 'columns')
+    # print(dailyDepositsAmountsUSD)
+    
+    # add repay type to the dataframe
+    repays = df[df['type'] == "repay"]
+    dailyRepaysAmountsUSD = repays.groupby([repays['DateTime'].dt.date]).sum()
+    dailyRepaysAmountsUSD['amountRepaysUSD'] = dailyRepaysAmountsUSD['amountUSD']
+    dailyRepaysAmountsUSD = dailyRepaysAmountsUSD.filter(items = ['DateTime', 'amountRepaysUSD'], axis = 'columns')
+    # print(dailyRepaysAmountsUSD)
+    
+    # add withdraw type to the dataframe
+    withdraws = df[df['type'] == "withdraw"]
+    dailyWithdrawsAmountsUSD = withdraws.groupby([withdraws['DateTime'].dt.date]).sum()
+    dailyWithdrawsAmountsUSD['amountWithdrawsUSD'] = dailyWithdrawsAmountsUSD['amountUSD']
+    dailyWithdrawsAmountsUSD = dailyWithdrawsAmountsUSD.filter(items = ['DateTime', 'amountWithdrawsUSD'], axis = 'columns')
+    # print(dailyWithdrawsAmountsUSD)
+    
+    dailyTransactionCount = dailyTransactionCount[['id']]
+    dailyTransactionCount.rename(columns={"id": "transactionCount"}, inplace = True)
+    # print(dailyTransactionCount)
+    
+    # We load the minutely Aave price data here:
+    aavePrices = pandas.read_csv('/data/IDEA_DeFi_Research/Data/Coin_Prices/Minutely/aavePrices.csv')
+    # And here, since we want to predict daily prices, we create a new features which is the mean daily price.
+    aavePrices['DateTime'] = aavePrices['timestamp'].transform(lambda x: datetime.fromtimestamp(x))
+    dailyMeanPrices = aavePrices.groupby([df['DateTime'].dt.date]).mean()
+    dailyMeanPrices = dailyMeanPrices[['priceUSD']]
+    # print(dailyMeanPrices)
+    
+    # feature engineering, merge deposit, repay, withdraw, borrow, liquidation in dailyTransactionCount
+    dailyTransactionCount = dailyTransactionCount.merge(dailyMeanPrices, how = "left", on = "DateTime")
+    dailyTransactionCount = dailyTransactionCount.merge(dailyBorrowsAmountsUSD, how = "left", on = "DateTime")
+    dailyTransactionCount = dailyTransactionCount.merge(dailyDepositsAmountsUSD, how = "left", on = "DateTime")
+    dailyTransactionCount = dailyTransactionCount.merge(dailyRepaysAmountsUSD, how = "left", on = "DateTime")
+    dailyTransactionCount = dailyTransactionCount.merge(dailyWithdrawsAmountsUSD, how = "left", on = "DateTime")
+    dailyTransactionCount = dailyTransactionCount.merge(liquidations, how = "left", on = "DateTime")
+    
+    # replace all NaN data to 0
+    dailyTransactionCount.fillna(0, inplace=True)
+    print(dailyTransactionCount)
+    
+    return dailyTransactionCount
