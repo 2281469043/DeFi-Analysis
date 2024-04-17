97dc9cff87a720b7f76f29adf88d68b99f89733e
Kaiyang Chang <changk2@rpi.edu>
2024-04-17 04:25:06 -0400

    Update v3_Ar.py

--- src/changk2/group_final_report/v3_Ar.py
+++ src/changk2/group_final_report/v3_Ar.py
@@ -75,3 +75,63 @@ def transaction_v3_Arbitrum():
     print(dailyTransactionCount)
     
     return dailyTransactionCount
+
+def data_split(data_set):
+    from sklearn.model_selection import TimeSeriesSplit
+    # We want to use the transactionCount to predict the next day's price. To do this, we "lead" the priceUSD
+    # column so in a given row, the transaction count is aligned with the next day's price.
+    data_set['priceUSD_lead_1'] = data_set['priceUSD'].shift(-1)
+    # We need to drop NA values. One NA value is introduced through this "lead" on the last day in the dataset.
+    data_set.dropna(inplace=True)
+    # In practice, it is better to predict daily percent price changes rather than predicting literal prices, so we compute the daily
+    # percent change here by subtraction tomorrow's price from today's and dividing by today's price.
+    data_set['dailyPercentChange'] = (data_set['priceUSD_lead_1'] - data_set['priceUSD']) / data_set['priceUSD']
+    # We want to predict the direction of the daily percent change, so we create a new feature which is the sign of the daily percent change.
+    data_set['directionOfDailyChange'] = np.sign(data_set['dailyPercentChange'])
+    # print(data_set)
+    tss = TimeSeriesSplit(n_splits = 3)
+    X = data_set.drop(labels=['priceUSD_lead_1', 'dailyPercentChange', 'directionOfDailyChange'],axis=1)
+    y = data_set['directionOfDailyChange']
+    for train_index, test_index in tss.split(data_set):
+        feature_train, feature_test = X.iloc[train_index, :], X.iloc[test_index,:]
+        target_train, target_test = y.iloc[train_index], y.iloc[test_index]
+    return [feature_train, feature_test, target_train, target_test]
+
+def plot_ground_truth(predictions, target_test_vals):
+    # We plot the ground-truth values in blue and the predicted values in red:
+    plt.plot(target_test_vals, color = "lightblue")
+    plt.plot(predictions, color = "lightpink")
+
+def plot_difference(predictions, y_test_vals):
+    # We plot the difference between our model's predictions and the actual values:
+    plt.plot(y_test_vals - predictions)
+
+machine_learning_model_record = dict()
+
+def logistic_regression_model(feature_train, feature_test, target_train, target_test):
+    from sklearn.linear_model import LogisticRegression
+    estimator = LogisticRegression(C = 1.0, penalty = "l2", solver = "liblinear", fit_intercept=True, max_iter=1000)
+    fit = estimator.fit(feature_train, target_train)
+    # We compute the predictions for the feature_test features:
+    predictions = fit.predict(feature_test)
+    # The line below just computes the average accuracy of our predictions:
+    np.linalg.norm(predictions - target_test) / len(target_test)
+    target_test_vals = list()
+    for data in target_test:
+        target_test_vals.append(data)
+    # model evaluation
+    target_predict = estimator.predict(feature_test)
+    print("-------------------- logistic regression --------------------\n")
+    print("The target_predict is:\n", target_predict)
+    print("Compare predicted results with actual values:\n", target_predict == target_test)
+    accuracy = estimator.score(feature_test, target_test) * 100
+    print("Accuracy:\n{0:.2f}%".format(accuracy))
+    return predictions, target_test_vals, accuracy
+
+dailyTransactionCount_v3_Arbitrum = transaction_v3_Arbitrum()
+feature_train, feature_test, target_train, target_test = data_split(dailyTransactionCount_v3_Arbitrum)
+predictions, target_test_vals, accuracy = logistic_regression_model(feature_train, feature_test, target_train, target_test)
+# make record for the accuracy
+machine_learning_model_record["logistic_regression"] = accuracy
+plot_ground_truth(predictions, target_test_vals)
+plot_difference(predictions, target_test_vals)
